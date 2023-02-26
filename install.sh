dbccc04b9e7483cebcf42e90fdd22d4e630971a2
GnarLito <gnarlito35@gmail.com>
2023-02-26 16:38:26 +0100

    [install.sh] run scripts in parallel in the background

--- install.sh
+++ install.sh
@@ -1,22 +1,32 @@
 #! /bin/bash
 
 
+
+load_script() {
+         output=$(bash $1 load)
+	 if [ $? -eq 0 ]
+	 then
+		 printf "    - Loaded: ${1##*/}\n" 
+	 else
+		 printf "    \033[0;31m- Failed\033[0m: ${1##*/} %-15s- $output\n"
+	 fi
+}
+
 load_scripts() {
-	echo "Loading all scripts from ($1)"
+	printf "Loading all scripts from ($1)\n"
 	for FILE in $1/*.sh; do 
-		 bash $FILE load
-		 if [ $? -eq 0 ]
-		 then
-			 echo -e "    - Loaded: ${FILE##*/}"
-		 else
-			 echo -e "    \033[0;31m- Failed\033[0m: ${FILE##*/}"
-		fi
-	 done
+		load_script $FILE &
+		jobs[$!]=$!
+	done
+
+	for PID in ${jobs[*]}; do
+		wait $PID
+	done
 }
 
 load_scripts "/opt/linux_essentials/scripts"
-echo -e "\nAll scripts loaded, please reload the profile file:
-	
-	. ~/.profile
+printf "\nAll scripts loaded, please reload the profile file:
+
+    source ~/.profile
 
 "
